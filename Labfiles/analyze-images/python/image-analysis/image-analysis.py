98656c009ed7f5afe489ebf1384d3b56d73bf126
RJ <Ranjan_wit@yahoo.co.in>
2026-02-22 15:21:52 +0100

    test

--- Labfiles/analyze-images/python/image-analysis/image-analysis.py
+++ Labfiles/analyze-images/python/image-analysis/image-analysis.py
@@ -7,6 +7,9 @@ from azure.core.exceptions import HttpResponseError
 import requests
 
 # import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
 
 
 def main():
@@ -27,22 +30,53 @@ def main():
         
 
         # Authenticate Azure AI Vision client
-
-
-        # Analyze image
-
-
-        # Get image captions
+        cv_client=ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key)
+        )  
         
-
+        # Analyze image
+        with open(image_file, "rb") as f:
+            image_data = f.read()
+        print(f"Analyzing image: {image_file}...\n")
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[
+                VisualFeatures.CAPTION,
+                VisualFeatures.TAGS,
+                VisualFeatures.OBJECTS,
+                VisualFeatures.PEOPLE,
+                VisualFeatures.DENSE_CAPTIONS,
+            ],
+        )
+        # Get image captions
+        if result.caption is not None:
+            print("Image captions:")
+            print(" Caption: '{}' (confidence: {:.2f}%)".format(result.caption.text, result.caption.confidence * 100))
+        
+        if result.dense_captions is not None:
+            print("\nDense Captions:")
+            for caption in result.dense_captions.list:
+                print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
         # Get image tags
-
+        if result.tags is not None:
+            print("\nImage tags:")
+            for tag in result.tags.list:
+                print(" Tag: '{}' (confidence: {:.2f}%)".format(tag.name, tag.confidence * 100))
 
         # Get objects in the image
-
+        if result.objects is not None:
+            print("\nObjects detected in the image:")
+            for detected_object in result.objects.list:
+                print(" {} (confidence: {:.2f}%)".format(detected_object.tags[0].name, detected_object.tags[0].confidence * 100))
+            show_objects(image_file, result.objects.list)
 
         # Get people in the image
-  
+        if result.people is not None:
+            print("\nPeople detected in the image:")
+            for detected_person in result.people.list:
+                print(" Person (confidence: {:.2f}%)".format(detected_person.confidence * 100))
+            show_people(image_file, result.people.list)
             
         
     except Exception as ex:
